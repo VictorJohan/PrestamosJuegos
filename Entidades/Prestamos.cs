b351557313eabe033c9f0c82d253c5c0908cf1cf
Victor Johan <vicjohan23@gmail.com>
2020-08-11 22:01:56 -0400

    SE CREARON LAS SIGUIENTES ENTIDADES: -Amigos. -Entradas. -Juegos. -Prestamos. -PrestamosDetalle.

---
+++ Entidades/Prestamos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace PrestamosJuegos.Entidades
+{
+    public class Prestamos
+    {
+        [Key]
+        public int PrestamoId { get; set; }
+        public DateTime Fecha { get; set; }
+        public int AmigoId { get; set; }
+        public string Observacion { get; set; }
+        public int CantidadJuegos { get; set; }
+
+        [ForeignKey("AmigoId")]
+        public virtual Amigos Amigo { get; set; }
+
+        [ForeignKey("PrestamoId")]
+        public virtual List<PrestamosDetalle> PrestamosDetalles { get; set; }
+
+    }
+}
---
+++ Entidades/PrestamosDetalle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace PrestamosJuegos.Entidades
+{
+    public class PrestamosDetalle
+    {
+        [Key]
+        public int Id { get; set; }
+        public int PrestamoId { get; set; }
+        public int Cantidad { get; set; }
+        public int JuegoId { get; set; }
+
+        [ForeignKey("JuegoId")]
+        public virtual Juegos Juego { get; set; }
+    }
+}
