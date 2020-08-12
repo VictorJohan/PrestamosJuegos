67f928d992d91f08b36bad8d7c03d17456e5b5dd
Victor Johan <vicjohan23@gmail.com>
2020-08-11 23:54:49 -0400

    SE CREARON LAS SIGUIENTES BLL: -AmigosBLL -EntradasBLL -JuegosBLL -PrestamosBLL

---
+++ BLL/AmigosBLL.cs
@@ -0,0 +1,183 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrestamosJuegos.DAL;
+using PrestamosJuegos.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace PrestamosJuegos.BLL
+{
+    public class AmigosBLL
+    {
+        public static bool Guardar(Amigos amigo)
+        {
+            if (!Existe(amigo.AmigoId))
+                return Insertar(amigo);
+            else
+                return Modificar(amigo);
+        }
+
+        public static bool Existe(int id)
+        {
+            Contexto contexto = new Contexto();
+            bool ok = false;
+
+            try
+            {
+                ok = contexto.Amigos.Any(a => a.AmigoId == id);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return ok;
+        }
+
+        private static bool Insertar(Amigos amigo)
+        {
+            Contexto contexto = new Contexto();
+            bool ok = false;
+
+            try
+            {
+                contexto.Amigos.Add(amigo);
+                ok = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return ok;
+        }
+
+        private static bool Modificar(Amigos amigo)
+        {
+            Contexto contexto = new Contexto();
+            bool ok = false;
+
+            try
+            {
+                contexto.Entry(amigo).State = EntityState.Modified;
+                ok = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return ok;
+        }
+
+        public static Amigos Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            Amigos amigo;
+
+            try
+            {
+                amigo = contexto.Amigos.Find(id);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return amigo;
+        }
+
+        public static bool Eliminar(int id)
+        {
+            Contexto contexto = new Contexto();
+            bool ok = false;
+
+            try
+            {
+                var eliminar = contexto.Amigos.Find(id);
+                if(eliminar != null)
+                {
+                    contexto.Amigos.Remove(eliminar);
+                    ok = contexto.SaveChanges() > 0;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return ok;
+        }
+
+        public static List<Amigos> GetAmigos()
+        {
+            Contexto contexto = new Contexto();
+            List<Amigos> lista = new List<Amigos>();
+
+            try
+            {
+                lista = contexto.Amigos.ToList();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return lista;
+        }
+
+        public static List<Amigos> GetAmigos(Expression<Func<Amigos, bool>> criterio)
+        {
+            Contexto contexto = new Contexto();
+            List<Amigos> lista = new List<Amigos>();
+
+            try
+            {
+                lista = contexto.Amigos.Where(criterio).ToList();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return lista;
+        }
+    }
+}
---
+++ BLL/PrestamosBLL.cs
@@ -0,0 +1,167 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrestamosJuegos.DAL;
+using PrestamosJuegos.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace PrestamosJuegos.BLL
+{
+    class PrestamosBLL
+    {
+        public static bool Guardar(Prestamos prestamo)
+        {
+            if (!Existe(prestamo.PrestamoId))
+                return Insertar(prestamo);
+            else
+                return Modificar(prestamo);
+        }
+
+        public static bool Existe(int id)
+        {
+            Contexto contexto = new Contexto();
+            bool ok = false;
+
+            try
+            {
+                ok = contexto.Prestamos.Any(p => p.PrestamoId == id);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return ok;
+        }
+
+        private static bool Insertar(Prestamos prestamo)
+        {
+            Contexto contexto = new Contexto();
+            bool ok = false;
+
+            try
+            {
+                contexto.Prestamos.Add(prestamo);
+                ok = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return ok;
+        }
+
+        private static bool Modificar(Prestamos prestamo)
+        {
+            Contexto contexto = new Contexto();
+            bool ok = false;
+            try
+            {
+                contexto.Database.ExecuteSqlRaw($"Delete FROM PrestamoDetalle Where PrestamoId={prestamo.PrestamoId}");
+                foreach (var item in prestamo.PrestamosDetalles)
+                {
+                    contexto.Entry(item).State = EntityState.Added;
+                }
+                contexto.Entry(prestamo).State = EntityState.Modified;
+                ok = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return ok;
+        }
+
+        public static Prestamos Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            Prestamos prestamo;
+
+            try
+            {
+                prestamo = contexto.Prestamos.Where(p => p.PrestamoId == id).Include(p => p.PrestamosDetalles)
+                    .ThenInclude(d => d.Juego).SingleOrDefault();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return prestamo;
+        }
+
+        public static bool Eliminar(int id)
+        {
+            Contexto contexto = new Contexto();
+            bool ok = false;
+
+            try
+            {
+                var eliminar = contexto.Prestamos.Find(id);
+                if(eliminar != null)
+                {
+                    contexto.Entry(eliminar).State = EntityState.Deleted;
+                    ok = contexto.SaveChanges() > 0;
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return ok;
+        }
+
+        public static List<Prestamos> GetPrestamos(Expression<Func<Prestamos, bool>> criterio)
+        {
+            Contexto contexto = new Contexto();
+            List<Prestamos> lista = new List<Prestamos>();
+
+            try
+            {
+                lista = contexto.Prestamos.Where(criterio).ToList();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return lista;
+        }
+    }
+}
