974f3eec4793bfe149361cb91a233b236509feeb
Victor Johan <vicjohan23@gmail.com>
2020-08-11 22:17:05 -0400

    SE CREO LA CLASE Contexto Y LA BASE DE DATOS.

---
+++ DAL/Contexto.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrestamosJuegos.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+
+namespace PrestamosJuegos.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Amigos> Amigos { get; set; }
+        public DbSet<Juegos> Juegos { get; set; }
+        public DbSet<Entradas> Entradas { get; set; }
+        public DbSet<Prestamos> Prestamos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source= DATA\PrestamosJuegos.db");
+        }
+    }
+}
